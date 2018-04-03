74d81cc3ae6570d41c76d26ed3ae774a1257b568
liudan <liudan@huimin100.cn>
2018-04-03 23:22:56 +0800

    Initial commit

---
+++ src/main/java/com/example/gitup/GitupApplication.java
@@ -0,0 +1,12 @@
+package com.example.gitup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitupApplication.class, args);
+	}
+}
